abe27730913d521822572db096f7c5d746891e40
jhancock <jessehancock1@gmail.com>
2018-01-31 19:59:15 -0700

    little change

--- exercises/baby_steps/program.js
+++ exercises/baby_steps/program.js
@@ -2,7 +2,6 @@ var length = +process.argv.length
 var total = 0
 
 for(var i = 2; i < length; i++){
-  // console.log(process.argv[i])
   total = total + Number(process.argv[i])
 }
  console.log(total)
